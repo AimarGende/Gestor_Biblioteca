3f25e675da1cf52bbbca4a455531606805eb6fb1
Aimar Gende <Aimar Gende@HZ114821.H012108.NET>
2023-01-30 13:21:36 +0100

    private con

--- src/packLibreria/Conector.java
+++ src/packLibreria/Conector.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class Conector {
-	Connection con;
+	private Connection con;
 	
 	public void  conectar() throws ClassNotFoundException, SQLException {
 		System.out.println("Conectando");
