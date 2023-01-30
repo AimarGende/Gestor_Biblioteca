66ff199ad2c641782af67d6161e829126e1fe239
Aimar Gende <Aimar Gende@HZ114821.H012108.NET>
2023-01-30 13:19:16 +0100

    metodos definidos

---
+++ src/packLibreria/GestorBBDD.java
@@ -0,0 +1,15 @@
+package packLibreria;
+
+public class GestorBBDD {
+	public void insertarLibro(Libro libro) {
+		
+	}
+	
+	public void eliminarLibro(int id) {
+		
+	}
+	
+	public Libro getLibro(int id) {
+		
+	}
+}
