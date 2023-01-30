79a35dea0a790002e1dae6970d8ec1d3a3e0aa84
Aimar Gende <Aimar Gende@HZ114821.H012108.NET>
2023-01-30 13:42:55 +0100

    Gestor insertar y eliminar

--- src/packLibreria/GestorBBDD.java
+++ src/packLibreria/GestorBBDD.java
@@ -1,15 +1,41 @@
 package packLibreria;
 
-public class GestorBBDD {
-	public void insertarLibro(Libro libro) {
-		
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class GestorBBDD extends Conector{
+	PreparedStatement pt;
+	public void insertarLibro(Libro libro) throws ClassNotFoundException, SQLException {
+		conectar();
+		pt = getCon().prepareStatement("INSERT INTO libros (titulo, autor, num_pag) VALUES (?,?,?)");
+		pt.setString(1, libro.getTitulo());
+		pt.setString(2,libro.getAutor());
+		pt.setInt(3, libro.getNum_pag());
+		cerrar();
 	}
 	
-	public void eliminarLibro(int id) {
-		
+	public void eliminarLibro(int id) throws ClassNotFoundException, SQLException {
+		conectar();
+		pt = getCon().prepareStatement("DELETE FROM libros WHERE id=?");
+		pt.setInt(1, id);
+		cerrar();
+
 	}
 	
-	public Libro getLibro(int id) {
+	public Libro getLibro(int id) throws SQLException {
+		String select="SELECT * FROM libros WHERE id=?";
+		Libro libro=new Libro();
 		
+		pt=getCon().prepareStatement(select);
+		pt.setInt(1, id);
+		ResultSet result=pt.executeQuery();
+		
+		libro.setId(result.getInt("id"));
+		libro.setTitulo(result.getString("titulo"));
+		libro.setAutor(result.getString("autor"));
+		libro.setNum_pag(result.getInt("num_pag"));
+		
+		return libro;
 	}
 }
