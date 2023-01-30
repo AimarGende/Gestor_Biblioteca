1274573b55e1a673705abdf97923ad450d183383
Aimar Gende <Aimar Gende@HZ114821.H012108.NET>
2023-01-30 12:52:00 +0100

    Menu base

---
+++ src/packLibreria/Menu.java
@@ -0,0 +1,25 @@
+package packLibreria;
+
+public class Menu {
+	static void mostrarMenuPrincipal() {
+		System.out.println("1. Gestionar Libros");
+		System.out.println("2. Gestionar Socios");
+		System.out.println("3. Gestionar Prestamos");
+	}
+
+	static void mostrarMenuLibros() {
+		System.out.println("1. Insertar Libro");
+		System.out.println("2. Eliminar Libro");
+		System.out.println("3. Ver libros");
+	}
+
+	static void mostrarMenuSocios() {
+		System.out.println("1. Ver Socios");
+
+	}
+
+	static void mostrarMenuPrestamos() {
+		System.out.println("1. Realizar préstamo");
+
+	}
+}
