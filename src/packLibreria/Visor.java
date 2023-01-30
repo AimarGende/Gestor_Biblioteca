6cad4901d767ddaaaf029e1fb805ae18b203cb5e
Aimar Gende <Aimar Gende@HZ114821.H012108.NET>
2023-01-30 13:54:20 +0100

    Visor acabado

--- src/packLibreria/Visor.java
+++ src/packLibreria/Visor.java
@@ -4,6 +4,16 @@ import java.util.ArrayList;
 
 public class Visor {
 	static void mostrarLibros(ArrayList<Libro> libros) {
-		
+		for (Libro libro : libros) {
+			libro.toString();
+		}
+	}
+	
+	static void mostrarLibro(Libro libro) {
+		libro.toString();
+	}
+	
+	static void mostrarMensaje(String mensaje) {
+		System.out.println(mensaje);
 	}
 }
