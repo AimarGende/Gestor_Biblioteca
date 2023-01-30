a9e0d1f1f2361f2b4f4a8294f239b108050f10e7
Aimar Gende <Aimar Gende@HZ114821.H012108.NET>
2023-01-30 14:21:23 +0100

    Acabado

--- src/packLibreria/GestorLibros.java
+++ src/packLibreria/GestorLibros.java
@@ -4,6 +4,26 @@ import java.util.Scanner;
 
 public class GestorLibros {
 	static void run(Scanner sc) {
-		
+		int opcion=0;
+		do {
+			Menu.mostrarMenuLibros();
+			opcion=Integer.parseInt(sc.nextLine());
+			switch (opcion){
+				case 1:
+					System.out.println("Ha escogido insertar libro");
+					break;
+				case 2:
+					System.out.println("Ha escogido eliminar libro");
+					break;
+				case 3:
+					System.out.println("Ha escogido ver libros");
+					break;
+				case 0:
+					break;
+				default:
+					System.out.println("Opcion equivocada");
+					break;
+			}
+		}while(opcion!=0);
 	}
 }
