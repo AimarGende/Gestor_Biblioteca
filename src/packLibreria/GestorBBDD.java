9374106e77b3fdc5ac556570e3de6edada2b25da
Aimar Gende <Aimar Gende@HZ114821.H012108.NET>
2023-01-31 08:54:19 +0100

    Añadido metodo modificar libro

--- src/packLibreria/GestorBBDD.java
+++ src/packLibreria/GestorBBDD.java
@@ -3,10 +3,11 @@ package packLibreria;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class GestorBBDD extends Conector{
 	PreparedStatement pt;
-	 
+	Scanner sc=new Scanner(System.in); 
 	public GestorBBDD() {
 	}
 
@@ -25,6 +26,17 @@ public class GestorBBDD extends Conector{
 		pt.setInt(1, id);
 		pt.execute();
 	
+	}
+	
+	public void modificarLibro(int id) throws SQLException {
+		pt=getCon().prepareStatement("UPDATE libros SET titulo=?, autor=?, num_pag=? WHERE id=?");
+		Libro libro=getLibro(id);
+		libro=FormulariosDeDatos.modificarDatosLibro(libro, sc);
+		pt.setString(1,libro.getTitulo());
+		pt.setString(2, libro.getAutor());
+		pt.setInt(3, libro.getNum_pag());
+		pt.setInt(4, id);
+		pt.executeUpdate();
 	}
 	
 	public Libro getLibro(int id) throws SQLException {
