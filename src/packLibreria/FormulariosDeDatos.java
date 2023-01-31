d0bdf0fc65a48ce9b5af68bfc6fd4678bbfdf88b
Aimar Gende <Aimar Gende@HZ114821.H012108.NET>
2023-01-31 08:21:01 +0100

    Metodos definidos

---
+++ src/packLibreria/FormulariosDeDatos.java
@@ -0,0 +1,35 @@
+package packLibreria;
+
+import java.util.Scanner;
+
+public class FormulariosDeDatos {
+	
+	static Libro pedirDatos(Scanner sc) {
+		Libro libro=new Libro();
+		System.out.println("Insertar datos del libro:");
+		System.out.println("Inserte el titulo del libro:");
+		libro.setTitulo(sc.nextLine());
+		System.out.println("Inserte el autor del libro:");
+		libro.setAutor(sc.nextLine());
+		System.out.println("Inserte el numero de paginas del libro");
+		libro.setNum_pag(Integer.parseInt(sc.nextLine()));
+		System.out.println("Ha insertado los datos, muchas gracias.");
+		return libro;
+	}
+	
+	static Libro modificarDatosLibro(Libro libro, Scanner sc) {
+		System.out.println("Modificar datos del libro:");
+		System.out.println("Inserte el nuevo titulo del libro:");
+		libro.setTitulo(sc.nextLine());
+		System.out.println("Inserte el nuevo autor del libro:");
+		libro.setAutor(sc.nextLine());
+		System.out.println("Inserte el nuevo numero de paginas del libro");
+		libro.setNum_pag(Integer.parseInt(sc.nextLine()));
+		System.out.println("Ha insertado los datos, muchas gracias.");
+		return libro;
+	}
+	
+	static int pedirIdLibro(Scanner sc) {
+		return 0;
+	}
+}
