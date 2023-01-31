bf70db28a11894a3e10feee4568bc9f960211e5d
Aimar Gende <Aimar Gende@HZ114821.H012108.NET>
2023-01-31 09:19:33 +0100

    acabado, revisar

--- src/packLibreria/GestorLibros.java
+++ src/packLibreria/GestorLibros.java
@@ -1,31 +1,54 @@
 package packLibreria;
 
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class GestorLibros {
-	static void run(Scanner sc) {
+	static void run(Scanner sc) throws ClassNotFoundException, SQLException {
 		int opcion=0;
+		int id=0;
+		Libro libro=new Libro();
+		GestorBBDD gest=new GestorBBDD();
 		do {
 			Menu.mostrarMenuLibros();
 			opcion=Integer.parseInt(sc.nextLine());
 			switch (opcion){
 				case Menu.INSERTAR_LIBRO:
 					System.out.println("Ha escogido insertar libro");
+					
+					libro=FormulariosDeDatos.pedirDatos(sc);
+					gest.conectar();
+					gest.insertarLibro(libro);
+					gest.cerrar();
+					
 					break;
 				case Menu.ELIMINAR_LIBRO:
 					System.out.println("Ha escogido eliminar libro");
+					id=FormulariosDeDatos.pedirIdLibro(sc);
+					gest.conectar();
+					gest.eliminarLibro(id);
+					gest.cerrar();
 					break;
 				case Menu.MODIFICAR_LIBRO:
 					System.out.println("Ha escogido modificar libro");
+					id=FormulariosDeDatos.pedirIdLibro(sc);
+					gest.conectar();
+					libro=gest.getLibro(id);
+					libro=FormulariosDeDatos.modificarDatosLibro(libro, sc);
+					gest.modificarLibro(libro, id,sc);
+					gest.cerrar();
 					break;
 				case Menu.VER_LIBROS:
 					System.out.println("Ha escogido ver libros");
+					gest.conectar();
+					Visor.mostrarLibros(gest.getLibros());
+					gest.cerrar();
 					break;
 				case Menu.SALIR:
 					System.out.println("Saliendo del menu...");
 					break;
 				default:
-					System.out.println("Opcion equivocada");
+					System.out.println("Opcion equivocada, insertelo otra vez");
 					break;
 			}
 		}while(opcion!=Menu.SALIR);
