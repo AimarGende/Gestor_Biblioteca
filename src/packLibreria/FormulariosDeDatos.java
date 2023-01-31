33ffe68f37a8e5d37331783aefdc6c647d90eceb
Aimar Gende <Aimar Gende@HZ114821.H012108.NET>
2023-01-31 09:55:21 +0100

    Datos socio

--- src/packLibreria/FormulariosDeDatos.java
+++ src/packLibreria/FormulariosDeDatos.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class FormulariosDeDatos {
 	
-	static Libro pedirDatos(Scanner sc) {
+	static Libro pedirDatosLibro(Scanner sc) {
 		Libro libro=new Libro();
 		System.out.println("Insertar datos del libro:");
 		System.out.println("Inserte el titulo del libro:");
@@ -37,4 +37,47 @@ public class FormulariosDeDatos {
 	}
 	
 //hacer seccion para socios
+	
+	static Socios pedirDatosSocio(Scanner sc) {
+		Socios socio=new Socios();
+		System.out.println("Insertar datos del socio:");
+		System.out.println("Inserte el nombre del socio:");
+		socio.setNombre(sc.nextLine());
+		System.out.println("Inserte el apellido del socio");
+		socio.setApellido(sc.nextLine());
+		System.out.println("Inserte la direccion del socio");
+		socio.setDireccion(sc.nextLine());
+		System.out.println("Inserte la poblacion donde vive del socio");
+		socio.setPoblacion(sc.nextLine());
+		System.out.println("Inserte la provincia donde vive el socio");
+		socio.setProvincia(sc.nextLine());
+		System.out.println("Inserte el dni del socio");
+		socio.setDni(sc.nextLine());
+		return socio;
+	}
+	
+	static Socios modificarDatosSocio(Socios socio, Scanner sc) {
+		System.out.println("Modificar datos del socio:");
+		System.out.println("Insertar datos del socio:");
+		System.out.println("Inserte el nuevo nombre del socio:");
+		socio.setNombre(sc.nextLine());
+		System.out.println("Inserte el nuevo apellido del socio");
+		socio.setApellido(sc.nextLine());
+		System.out.println("Inserte la nuevo direccion del socio");
+		socio.setDireccion(sc.nextLine());
+		System.out.println("Inserte la nuevo poblacion donde vive del socio");
+		socio.setPoblacion(sc.nextLine());
+		System.out.println("Inserte la nuevo provincia donde vive el socio");
+		socio.setProvincia(sc.nextLine());
+		System.out.println("Inserte el nuevo dni del socio");
+		socio.setDni(sc.nextLine());
+		return socio;
+	}
+	
+	static int pedirIdSocio(Scanner sc) {
+		int id=0;
+		System.out.println("Inserte la id del socio");
+		id=Integer.parseInt(sc.nextLine());
+		return id;
+	}
 }
