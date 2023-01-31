0bb283e89297cb17e1a7ca470a54a546ecd41321
Aimar Gende <Aimar Gende@HZ114821.H012108.NET>
2023-01-31 09:21:07 +0100

    Definido, acabar

--- src/packLibreria/GestorBiblioteca.java
+++ src/packLibreria/GestorBiblioteca.java
@@ -1,7 +1,30 @@
 package packLibreria;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class GestorBiblioteca {
-	public static void run() {
-		
+	public static void run() throws ClassNotFoundException, SQLException {
+		Scanner sc=new Scanner(System.in);
+		int opcion=0;
+		do {
+			Menu.mostrarMenuPrincipal();
+			opcion=Integer.parseInt(sc.nextLine());
+			switch (opcion) {
+				case Menu.GESTIONAR_LIBROS:
+					GestorLibros.run(sc);
+					break;
+				case Menu.GESTIONAR_SOCIOS:
+					GestorSocios.run(sc);
+					break;
+				case Menu.GESTIONAR_PRESTAMOS:
+					break;
+				case Menu.SALIR:
+					break;
+				default:
+					break;
+			}
+		}while(opcion!=Menu.SALIR);
+		sc.close();
 	}
 }
