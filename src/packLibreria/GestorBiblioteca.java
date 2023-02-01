af0885330847613a8ebc8f5e630e212e5c973756
Aimar Gende <Aimar Gende@HZ114821.H012108.NET>
2023-02-01 12:40:23 +0100

    Visor mensaje

--- src/packLibreria/GestorBiblioteca.java
+++ src/packLibreria/GestorBiblioteca.java
@@ -26,6 +26,6 @@ public class GestorBiblioteca {
 			}
 		}while(opcion!=Menu.SALIR);
 		sc.close();
-		System.out.println("Muchas gracias por usar nuestra app, esperemos que vuelva pronto!");
+		Visor.mostrarMensaje("Muchas gracias por usar nuestra app, esperemos que vuelva pronto!");
 	}
 }
