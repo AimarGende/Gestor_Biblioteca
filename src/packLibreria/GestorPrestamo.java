1b17a4cc07fa7a58d9ebc9f847c715c97ad7fce3
Aimar Gende <Aimar Gende@HZ114821.H012108.NET>
2023-02-01 12:46:33 +0100

    Creado

---
+++ src/packLibreria/GestorPrestamo.java
@@ -0,0 +1,10 @@
+package packLibreria;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class GestorPrestamo {
+	static void run(Scanner sc) throws ClassNotFoundException, SQLException{
+		
+	}
+}
